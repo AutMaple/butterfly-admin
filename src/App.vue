82c52175e819b1ed95b77cab764cd57be77e234a
AutMaple <autmaple609@qq.com>
2023-06-11 10:47:42 +0800

    init commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <h1>Hello Vue</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+}
+</style>
